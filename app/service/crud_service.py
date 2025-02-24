5258f173db406506967a94f078ab24e27ff4e70a
tech-master823 <carlsh0823@gmail.com>
2025-02-24 05:59:13 +0300

    todo

--- app/service/crud_service.py
+++ app/service/crud_service.py
@@ -4,5 +4,8 @@ class TodoService:
     @staticmethod
     def get_all_todos() -> list[TodoModel]:
         return []
+    
+    @staticmethod
     def create_new_todo(todo: TodoModel):
+            
         return []
